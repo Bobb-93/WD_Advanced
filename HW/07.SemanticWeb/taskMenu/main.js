8f8d9a09588677aa5d81dca45fbfeb7af0c9ad52
Bobb-93 <borisiakimov@gmail.com>
2021-03-28 20:27:05 +0300

    taskMenu first solution

--- HW/07.SemanticWeb/taskMenu/main.js
+++ HW/07.SemanticWeb/taskMenu/main.js
@@ -1,6 +1,8 @@
 // TODO: set as HW
 const items = document.querySelectorAll('.menu li');
-const btnGetTotal = document.querySelector('.btnGetTotal');
+const btnCalcTotal = document.querySelector('.btnCalcTotal');
+const btnReset = document.querySelector('.btnReset');
+const totalPriceSpan = document.getElementById("totalPriceSpan");
 
 let totalPrice = 0;
 let currentPrices = [];
@@ -8,18 +10,37 @@ let currentPrices = [];
 // when a LI is clicked => toggle class 'choosen'
 for( item of items){
 	item.addEventListener('click', function (e) {
-		let item = e.target;
+		let item = e.currentTarget;
 		item.classList.toggle('choosen');
 
 		let price = item.firstElementChild.innerText*1;
 
-		currentPrices.push(price);
-		// currentPrices.push(-price);
+		if(item.classList.contains("choosen")){
+			currentPrices.push(price);
+		}else{
+			currentPrices.push(-price);
+		}
 	});
 }
 
-btnGetTotal.addEventListener('click', function (e) {
+btnCalcTotal.addEventListener('click', function () {
 	// get total sum of all choosen items
+	for (let i = 0; i < currentPrices.length; i++) {
+		const element = currentPrices[i];
+		totalPrice += element;
+	}
+	totalPriceSpan.innerText = totalPrice;
+	totalPrice = 0;
+});
 
-})
+btnReset.addEventListener('click', function() {
+	totalPrice = 0;
+	totalPriceSpan.innerText = totalPrice;
+	currentPrices = [];
 
+	for (item of items) {
+		if(item.classList.contains("choosen")){
+			item.classList.toggle('choosen');
+		}
+	}
+});
