9b7af23fb4d500ca3680741330830d140ca744b6
Bobb-93 <borisiakimov@gmail.com>
2021-03-29 21:41:27 +0300

    fixed hh:mm:ss

--- HW/07.SemanticWeb/musicPlayer/main.js
+++ HW/07.SemanticWeb/musicPlayer/main.js
@@ -1,16 +1,43 @@
+//variables
 const spanList = document.querySelectorAll("span");
 const liList = document.querySelectorAll("li");
+const totalPlayTimeSpan = document.getElementById("totalPlayTimeSpan");
+let totalPlayTime;
+
+//auxilary functions
+function toSeconds(timeToConvert){
+    let time = timeToConvert.split(':'); // split it at the colons
+    
+    // minutes are worth 60 seconds. Hours are worth 60 minutes.
+    let seconds = (+time[0]) * 60 * 60 + (+time[1]) * 60 + (+time[2]); 
+    return seconds;
+}
+
+function toMinutesColonSeconds(timeToConvert){
+    return new Date(timeToConvert * 1000).toISOString().substr(11, 8);
+}
 
 function filterByGenre(genre){
+    totalPlayTime = 0;
+    
     liList.forEach(function(listItem){
         if(genre === "all" || listItem.dataset.genre === genre){
             listItem.style.display = "list-item";
+            // console.log(listItem.dataset.trackLength);
+            // console.log(toSeconds(listItem.dataset.trackLength));
+            
+            totalPlayTime += toSeconds(listItem.dataset.trackLength);
+            
+            // console.log(totalPlayTime);
+            
         }else if(listItem.dataset.genre !== genre){
             listItem.style.display = "none";
         }
     });
+    totalPlayTimeSpan.innerText = toMinutesColonSeconds(totalPlayTime);
 }   
 
+//event listener
 spanList.forEach(function(element){
     element.addEventListener("click", function(){
         if(element.dataset.genre === "classical"){
@@ -22,9 +49,11 @@ spanList.forEach(function(element){
         }else if(element.dataset.genre === "newWave"){
             // console.log("newWave");
             filterByGenre("newWave");
-        }else{
+        }else if(element.dataset.genre === "all"){
             // console.log("all");
             filterByGenre("all");
         }
     });
 });
+
+filterByGenre("all");
