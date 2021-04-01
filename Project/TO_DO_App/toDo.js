b8b5946b4a627e27b3f2e61d40a737a8d98bb13d
Bobb-93 <borisiakimov@gmail.com>
2021-04-02 00:55:27 +0300

    worked on to do app, but found even more bugs

--- Project/TO_DO_App/toDo.js
+++ Project/TO_DO_App/toDo.js
@@ -1,28 +1,27 @@
 function addTask(){
-    const taskTitle = document.getElementById("taskTitle").value;
-    const taskDetails = document.getElementById("taskDetails").value;
-    const taskDate = document.getElementById("taskDate").value;
-    const taskTime = document.getElementById("taskTime").value;
-    const noTasks = document.getElementById("noTasks");
-    const tasksTable = document.getElementById("noTasks");
     if(taskTitle !== ``){
         console.log(`taskTitle = ${taskTitle}`);
         console.log(`taskDetails = ${taskDetails}`);
         console.log(`taskDate = ${taskDate}`);
         console.log(`taskTime = ${taskTime}`);
         
-        noTasks.style.display = "none";
+        myTasks.style.display = "block";
         console.log(5);
-        tasksTable.style.display = "block";
+        // tasksTable.style.display = "block";
     }
     
 }
-        
+
 const submitButton = document.getElementById("submitButton");
 const markAllAsCompleted = document.getElementById("markAllAsCompleted");
 const deleteAllCompleted = document.getElementById("deleteAllCompleted");
 const deleteAllTasks = document.getElementById("deleteAllTasks");
-
+const taskTitle = document.getElementById("taskTitle").value;
+const taskDetails = document.getElementById("taskDetails").value;
+const taskDate = document.getElementById("taskDate").value;
+const taskTime = document.getElementById("taskTime").value;
+const tasksTable = document.getElementById("tasksTable");
+const myTasks = document.getElementById("myTasks");
 
 submitButton.addEventListener("click", addTask);
 markAllAsCompleted.addEventListener("click", markAllAsCompleted);
