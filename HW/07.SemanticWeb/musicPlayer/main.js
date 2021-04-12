ac7689afc664f2aa7735c2e07cc4a71bbe0f70bd
Bobb-93 <borisiakimov@gmail.com>
2021-04-12 22:09:04 +0300

    player works with classical music

--- HW/07.SemanticWeb/musicPlayer/main.js
+++ HW/07.SemanticWeb/musicPlayer/main.js
@@ -1,9 +1,82 @@
 //variables
-const radioList = document.querySelectorAll("#genres input[type='radio']");
-const liList = document.querySelectorAll("li");
-const totalPlayTimeSpan = document.getElementById("totalPlayTimeSpan");
-const nowPlaying = document.querySelector("figcaption>span");
-const songs = document.getElementById("songs");
+const dom = {
+    radioList: document.querySelectorAll("#genres input[type='radio']"),
+    liList: document.querySelectorAll("li"),
+    totalPlayTimeSpan: document.getElementById("totalPlayTimeSpan"),
+    nowPlaying: document.querySelector("figcaption>span"),
+    songs: document.getElementById("songs"),
+    audioPlayer: document.getElementById("audio-player")
+};
+
+const songs = [
+    {
+        id: 1,
+        trackLength: "00:03:35",
+        songName: "Antonio Vivaldi - The Four Seasons, Op. 8, \"Spring\": Allegro",
+        songSrc: "audio/The Four Seasons, Op. 8, Spring Allegro.mp3"
+    },
+    {
+        id: 2,
+        trackLength: "00:05:01",
+        songName: "Richard Wagner - The Valkyrie: Ride of the Valkyries",
+        songSrc: "audio/Richard Wagner - Ride Of The Valkyries.mp3"
+    },
+    {
+        id: 3,
+        trackLength: "00:04:48",
+        songName: "AC/DC - Dirty Deeds Done Dirt Cheap",
+        songSrc: ""
+    },
+    {
+        id: 4,
+        trackLength: "00:04:04",
+        songName: "a-Ha - Take On Me",
+        songSrc: ""
+    },
+    {
+        id: 5,
+        trackLength: "00:05:41",
+        songName: "Deep Purple - Smoke On The Water",
+        songSrc: ""
+    },
+    {
+        id: 6,
+        trackLength: "00:04:29",
+        songName: "Fr&egrave;d&egrave;ric Chopin - Nocturne No. 2 In E-Flat Major, Op. 9",
+        songSrc: "audio/Chopin - Nocturne op.9 No.2.mp3"
+    },
+    {
+        id: 7,
+        trackLength: "00:04:08",
+        songName: "Blue Monday - New Order",
+        songSrc: ""
+    },
+    {
+        id: 8,
+        trackLength: "00:03:12",
+        songName: "Transmission - Joy Division",
+        songSrc: ""
+    },
+    {
+        id: 9,
+        trackLength: "00:04:39",
+        songName: "Nirvana - Smells Like Teen Spirit",
+        songSrc: ""
+    },
+    {
+        id: 10,
+        trackLength: "00:04:43",
+        songName: "The Cure - A Forest",
+        songSrc: ""
+    },
+    {
+        id: 11,
+        trackLength: "00:08:34",
+        songName: "Planets Suite for Orchestra Op. 32 Venus, Bringer Of Peace",
+        songSrc: "audio/Planets Suite for Orchestra Op. 32 Venus, Bringer Of Peace.mp3"
+    }
+];
+
 let totalPlayTime;
 
 //auxilary functions
@@ -21,7 +94,7 @@ function toMinutesColonSeconds(timeToConvert){
 function filterByGenre(genre){
     totalPlayTime = 0;
     
-    liList.forEach(function(listItem){
+    dom.liList.forEach(function(listItem){
         let listItemGenre  = listItem.dataset.genre;
         if(genre === "all" || listItemGenre === genre){
             listItem.style.display = "list-item";
@@ -30,11 +103,11 @@ function filterByGenre(genre){
             listItem.style.display = "none";
         }
     });
-    totalPlayTimeSpan.innerText = toMinutesColonSeconds(totalPlayTime);
+    dom.totalPlayTimeSpan.innerText = toMinutesColonSeconds(totalPlayTime);
 }   
 
 //event listener
-radioList.forEach(function(element){
+dom.radioList.forEach(function(element){
     element.addEventListener("click", function(){
         if(this.value === "classical"){
             filterByGenre("classical");
@@ -48,11 +121,24 @@ radioList.forEach(function(element){
     });
 });
 
-songs.addEventListener("click", function(){
+dom.songs.addEventListener("click", function(e){
     if(e.target.nodeName!=="LI"){
-        return 
+        return;
     };
 
+    let trackId = e.target.dataset.id;
+    console.log(trackId);
+
+    // console.log(songs.find(x => x.id = trackId).songSrc);
+    let song = songs.find(function (item) { 
+        // console.log(item.id);
+        return item.id === +trackId; 
+    });
+
+    // console.log(`${song.songSrc}`);
+
+    // console.log(dom.audioPlayer.src);
     
+    dom.audioPlayer.src = `${song.songSrc}`;
 });
 filterByGenre("all");
